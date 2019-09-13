ae9cbea0a2a16a38bd42b5793521abffdd82b31b
sjemling123 <55298890+sjemling123@users.noreply.github.com>
2019-09-13 15:45:23 -0500

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    
+    private Rigidbody2D rb2d;
+
+    public Text countText;
+    public Text winText;
+    private int count;
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+        count = 0;
+        winText.text = " ";
+        SetcountText();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetcountText();
+        }
+    }
+
+        void SetcountText()
+        {
+            countText.text = "Count:" + count.ToString();
+
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+
+     }
+     }
---
+++ Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    
+       
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+    }
+}
